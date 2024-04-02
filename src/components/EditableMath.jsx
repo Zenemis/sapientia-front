251abe304fc5efcde42917bd96f04621422decc2
Simon Ducamp <simon.ducamp@enseirb-matmeca.fr>
2024-04-02 11:03:35 +0200

    draft: input box

---
+++ src/components/EditableMath.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useRef } from 'react';
+
+const EditableMath = ({ defaultText="", renderFunc, inputSize=30 }) => {
+  const [text, setText] = useState(defaultText);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      {isHovered ? (
+        <input
+          type="text"
+          value={text}
+          onChange={handleChange}
+          style={{ width:  `${inputSize}px` }}
+        />
+      ) : (
+        <div>{renderFunc(text)}</div>
+      )}
+    </div>
+  );
+};
+
+export default EditableMath;
--- src/pages/exercises/001-MatrixDiagonalization/Answer.jsx
+++ src/pages/exercises/001-MatrixDiagonalization/Answer.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Radio, RadioGroup, FormControlLabel } from '@mui/material';
+import EditableMath from '../../../components/EditableMath';
+import { UNSAFE_DataRouterContext } from 'react-router-dom';
+import { MathJax } from 'better-react-mathjax';
 
 function Answer({ state, handleAnswer }) {
     let render;
     switch (state.id) {
-        case 1:
+        case 2:
             render = <div></div>;
             break;
-        case 0:
+        case 1:
             render = (
                 <RadioGroup
                     aria-label="answer"
@@ -25,6 +28,14 @@ function Answer({ state, handleAnswer }) {
                     <FormControlLabel value="Non" control={<Radio />} label="Non" />
                 </RadioGroup>
             );
+            break;
+        case 0 :
+            const renderFunc = (text) => {
+                if (text == null || text == undefined) return <MathJax>{`!!!`}</MathJax>;
+                if (text == "") return <MathJax>{`!!!`}</MathJax>;
+                return <MathJax>{text}</MathJax>
+            };
+            render = <EditableMath defaultText={""} renderFunc={renderFunc}/>;
             break;
         default:
             render = <div></div>;
